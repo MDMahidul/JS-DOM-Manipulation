8bea01899a3ac4f93f252a527865154921c80de5
MD. Mahidul Islam <47059408+MDMahidul@users.noreply.github.com>
2021-01-10 21:47:18 +0600

    delete book-list

---
+++ app.js
@@ -0,0 +1,12 @@
+const list = document.querySelector('#book-list ul');
+
+//delete books
+list.addEventListener('click',function(e){
+    if(e.target.className == 'delete'){
+        const li = e.target.parentElement;
+        list.removeChild(li);
+    }
+})
+
+
+//add book-list
