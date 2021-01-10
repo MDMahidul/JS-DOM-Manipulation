3b1c25dbf0fefeb23917577cfe468a0c725d8490
MD. Mahidul Islam <47059408+MDMahidul@users.noreply.github.com>
2021-01-10 22:59:06 +0600

    checkboxes and change events

--- app.js
+++ app.js
@@ -34,5 +34,14 @@ addForm.addEventListener('submit',(e)=>{
     li.appendChild(deleteBtn);
     list.appendChild(li);
 
-   
+})
+
+//hide books
+const hideBox = document.querySelector('#hide');
+hideBox.addEventListener('change',(e)=>{
+    if(hideBox.checked){
+        list.style.display="none";
+    }else{
+        list.style.display="initial";
+    }
 })
