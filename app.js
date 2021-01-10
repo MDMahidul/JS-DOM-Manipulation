14c57bb50db51edc6685caa466f58ce1d3f927da
MD. Mahidul Islam <47059408+MDMahidul@users.noreply.github.com>
2021-01-10 22:02:57 +0600

    Creating Elements

--- app.js
+++ app.js
@@ -10,3 +10,25 @@ list.addEventListener('click',function(e){
 
 
 //add book-list
+const addForm = document.forms['add-book'];
+
+addForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const value = addForm.querySelector('input[type="text"]').value;
+
+    //create elements
+    const li =document.createElement('li');
+    const bookName = document.createElement('span');
+    const deleteBtn =  document.createElement('span');
+
+    //add content
+    deleteBtn.textContent = 'delete';
+    bookName.textContent=value;
+
+    // append to document
+    li.appendChild(bookName);
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+
+   
+})
